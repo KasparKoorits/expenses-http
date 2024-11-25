c060d2fff816c205fd1596cbb1070c336c204647
KasparKoorits <kaspar.koorits@voco.ee>
2024-11-25 12:52:35 +0200

    Exercise 3: Using the surrounding component in other components

--- src/components/Expenses.js
+++ src/components/Expenses.js
@@ -1,12 +1,13 @@
 import "./Expenses.css";
 import ExpenseItem from "./ExpenseItem.js";
+import Card from "./Card.js";
 
 const Expenses = (props) => {
   return (
-    <div className="expenses">
+    <Card className="expenses">
       <ExpenseItem data={props.data[0]} />
       <ExpenseItem data={props.data[1]} />
-    </div>
+    </Card>
   );
 };
 
